c19960d996c1f665f573faa2409f5cc443981f54
Estefany Torres <157387111+estefanytorres31@users.noreply.github.com>
2024-02-29 09:31:34 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
 import routerUsuario from "./routers/UsuarioRoutes";
+import routerDescuento from "./routers/DescuentoRouter"
 import morgan from "morgan";
 
-const app= express();
+const app= express(); 
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(routerUsuario)
+app.use(routerDescuento)
 
 
 export default app
