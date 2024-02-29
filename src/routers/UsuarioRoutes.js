1bd89660cd55f6c7d846d595cf2813da20d37921
Estefany Torres <157387111+estefanytorres31@users.noreply.github.com>
2024-02-29 12:01:09 -0500

    Update UsuarioRoutes.js

--- src/routers/UsuarioRoutes.js
+++ src/routers/UsuarioRoutes.js
@@ -1,7 +1,12 @@
-import { crearUsuario,login } from '../controllers/UsuarioContolador';
+import { crearUsuario,login,enviarPIN,verificarPIN,cambiarPassword,listaPaises } from '../controllers/UsuarioContolador';
 import { Router } from 'express';
 
 const routerUsuario = Router();
 routerUsuario.post('/registro',crearUsuario);
+routerUsuario.get('/listaPaises',listaPaises)
 routerUsuario.post('/login',login)
-export default routerUsuario;
+routerUsuario.post('/enviarPin',enviarPIN)
+routerUsuario.post('/verificarPin',verificarPIN)
+routerUsuario.post('/cambiarPassword',cambiarPassword)
+
+export default routerUsuario; 
