1c5b01e1f85427f44e3e0ed4d768965309712ec3
Estefany Torres <estefanyt3101@gmail.com>
2024-03-01 14:29:55 -0500

    actualizado

--- src/controllers/UsuarioContolador.js
+++ src/controllers/UsuarioContolador.js
@@ -43,51 +43,69 @@ export const crearUsuario=async (req, res)=>{
       }
 };
 export const login = async (req, res) => {
-    try { 
-        const connection = await connect();
+  try {
+    const connection = await connect();
 
-        // Buscar al usuario por nombre de usuario
-        const [results] = await connection.execute(
-            "SELECT * FROM usuarios WHERE email = ?",
-            [req.body.email]
-        );
+    // Buscar al usuario por nombre de usuario
+    const [results] = await connection.execute(
+      "SELECT * FROM usuarios WHERE email = ?",
+      [req.body.email]
+    );
 
-        // Verificar si el usuario existe
-        if (results.length === 0) {
-            return res.status(401).json({ error: 'Nombre de usuario o contraseña incorrectos' });
-        }
-
-        // Verificar la contraseña
-        const usuario = results[0];
-        const match = await bcrypt.compare(req.body.password, usuario.password);
-        if (!match) {
-            return res.status(401).json({ error: 'Nombre de usuario o contraseña incorrectos' });
-        }
-        const [sesionResults] = await connection.execute(
-            "SELECT token FROM sesiones WHERE usuario_id = ? AND fecha_expiracion > NOW()",
-            [usuario.id]
-        );
-
-        // Si hay una sesión activa, devolver el token existente
-        if (sesionResults.length > 0) {
-            const token = sesionResults[0].token;
-            return res.json({ token });
-        }
-
-        // Generar token JWT
-        const token = jwt.sign({ id: usuario.id, email: usuario.email }, 'secreto_del_token', { expiresIn: '1h' });
-
-        await connection.execute(
-            "INSERT INTO sesiones (usuario_id, token, fecha_expiracion) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 1 HOUR))",
-            [usuario.id, token]
-        );
-
-        return res.json({ token });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Error del servidor' });
+    // Verificar si el usuario existe
+    if (results.length === 0) {
+      return res
+        .status(401)
+        .json({ error: "Nombre de usuario o contraseña incorrectos" });
     }
+
+    // Verificar la contraseña
+    const usuario = results[0];
+    const match = await bcrypt.compare(req.body.password, usuario.password);
+    if (!match) {
+      return res
+        .status(401)
+        .json({ error: "Nombre de usuario o contraseña incorrectos" });
+    }
+
+    // Generar token JWT
+    const token = jwt.sign(
+      { id: usuario.id, email: usuario.email },
+      "secreto_del_token"
+    );
+
+    await connection.execute(
+      "INSERT INTO sesiones (usuario_id, token) VALUES (?, ?)",
+      [usuario.id, token]
+    );
+
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error del servidor" });
+  }
 };
+
+export const logout = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Obtener el token de los headers
+
+    const decodedToken = jwt.verify(token, "secreto_del_token"); // Decodificar el token
+
+    const connection = await connect();
+
+    await connection.execute(
+      "DELETE FROM sesiones WHERE usuario_id = ? AND token = ?",
+      [decodedToken.id, token]
+    );
+
+    return res.json({ message: "Sesión cerrada exitosamente" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error del servidor" });
+  }
+};
+
 // Variables globales para almacenar temporalmente el PIN y el correo electrónico
 let temporaryPIN = "";
 let temporaryEmail = "";
